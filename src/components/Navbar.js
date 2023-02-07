3dae8826aa575a8cceff8660e6aefc1b5cf0b0b8
most111 <44298945+most111@users.noreply.github.com>
2023-02-07 22:57:13 +0000

    moved notes section and experimenting with authentication info

---
+++ src/components/Navbar.js
@@ -0,0 +1,51 @@
+import React from "react";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { SidebarData } from "./SidebarData";
+import "./Navbar.css";
+import { IconContext } from "react-icons";
+import { Button } from "@aws-amplify/ui-react";
+
+function Navbar(props) {
+
+  const [sidebar, setSidebar] = useState(false);
+  const showSidebar = () => setSidebar(!sidebar);
+  return (
+    <>
+      <IconContext.Provider value={{ color: "undefined" }}>
+        <div className="navbar">
+          <Link to="#" className="menu-bars">
+            <FaIcons.FaBars onClick={showSidebar} style={{color:"black"}} />
+          </Link>
+        </div>
+        <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+          <ul className="nav-menu-items" onClick={showSidebar}>
+            <li className="navbar-toggle">
+              <Link to="#" className="menu-bars">
+                <div className="close">
+                  <AiIcons.AiOutlineClose />
+                </div>
+              </Link>
+            </li>
+            {/* <li style={{color:"red"}}>{props.userData}</li> */}
+            <li style={{color:"red"}}><Button onClick={props.button}>Sign Out</Button></li>
+            {SidebarData.map((item, index) => {
+              return (
+                <li key={index} className={item.cName}>
+                  <Link to={item.path}>
+                    {item.icon}
+                    <span>{item.title}</span>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </IconContext.Provider>
+    </>
+  );
+}
+
+export default Navbar;
---
+++ src/components/SidebarData.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import * as IoIcons from "react-icons/io";
+
+export const SidebarData = [
+    {
+        title: 'Home',
+        path: '/home',
+        icon: <AiIcons.AiFillHome />,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Products',
+        path: '/home',
+        icon: <FaIcons.FaCartPlus />,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Teams',
+        path: '/teams',
+        icon: <IoIcons.IoMdPeople />,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Messages',
+        path: '/messages',
+        icon: <AiIcons.AiFillHome />,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Support',
+        path: '/support',
+        icon: <AiIcons.AiFillHome />,
+        cName: 'nav-text'
+    },
+]
